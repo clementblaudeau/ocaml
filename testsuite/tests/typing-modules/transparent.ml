d67b88d232b389c5ca917d5d6f3127561953c713
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-05-24 17:53:08 +0200

    Prevent inference of present static aliases via avoidance
    Through avoidance, present static aliases could be inferred. This commit removes
    the logic of [nondep_mty_with_presence], instead relying on a match on the
    result of avoidance to decide on presence. Therefore, avoidance can still infer
    static aliases, but the presence flag always correspond to the static/dynamic
    type of alias.

--- testsuite/tests/typing-modules/transparent.ml
+++ testsuite/tests/typing-modules/transparent.ml
@@ -39,6 +39,17 @@ module M : sig module X : sig end end
 module M' : sig module X = M.X [@@dynamic_alias]  end
 |}]
 
+(* Avoidance should introduce dynamic aliases *)
+module X0 = struct end
+module M = struct
+  open (struct module X1 = X0 [@@dynamic_alias] end)
+  module X2 = X1
+end
+[%%expect{|
+module X0 : sig end
+module M : sig module X2 = X0 [@@dynamic_alias]  end
+|}]
+
 (** 3. Subtyping *)
 
 (* Dynamic aliases are a subtype of static ones *)
